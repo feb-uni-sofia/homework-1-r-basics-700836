618ef947d4d4bce4d2f031e039cd9c04ce3d9a83
Plamena <pamkalaydzhieva@gmail.com>
2018-04-01 11:38:36 +0300

    Here we go.

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,26 @@
+# (a)
+x<-c(4,1,1,4)
+x
+# (b)
+y<-c(1,4)
+y
+# (c)
+z<-x-y
+z
+# R takes the shorter vector (y) and makes it as long as x - recycling; 
+# the result is a new vector (z) where each element of x is diminished with the correspondig element in y
+# (d)
+s<-c(x,y)
+s
+# (e)
+rep(s,10)
+length(rep(s,10))
+# (f)
+rep(s,each=3)
+# (g) i)seq
+seq(7,21,by=1)
+# (g) ii):
+7:21
+# (h)
+length(seq(7,21,by=1))
 
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,56 @@
-
+# (a)
+xmin<-c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmin
+xmax<-c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+xmax
+# (b)
+difference<-xmax-xmin
+difference
+dayNames <- c('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun')
+names(difference)<-dayNames
+difference[c('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun')]
+# (c)
+avgMin <- mean(xmin)
+avgMin
+avgMax <- mean(xmax)
+avgMax
+# (d)
+xmin < avgMin
+minBelowAvg <- xmin < avgMin
+minBelowAvg
+dayNames [minBelowAvg]
+# (e)
+xmax > avgMax
+maxAboveAvg <- xmax > avgMax
+maxAboveAvg
+dayNames [maxAboveAvg]
+# (f)
+names(xmin)<-dayNames
+xmin[c('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun')]
+names(xmax)<-dayNames
+xmax[c('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun')]
+# (g)
+temperatures <- data.frame(
+  df.xmin = xmin, 
+  df.xmax = xmax
+  )
+temperatures
+# (h)
+temperatures <- within(temperatures, {
+  xminFahrenheit <- df.xmin * 9/5 + 32
+})
+temperatures
+# (i)
+xminFahrenheit<-xmin * 9/5 + 32
+xmaxFahrenheit<-xmax * 9/5 + 32
+temperaturesFahr <- data.frame(
+  df.xminFahrenheit = xminFahrenheit,
+  df.xmaxFahrenheit = xmaxFahrenheit
+)  
+temperaturesFahr
+# (j) i)
+tempMonFri1<-data.frame(temperaturesFahr[c(1: 5), ])
+tempMonFri1
+# (j) ii)
+tempMonFri2<-data.frame(temperaturesFahr[-c(6, 7), ])
+tempMonFri2
